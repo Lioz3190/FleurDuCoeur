c61d8b2a953451730991eb637f813a4abc364ff7
Lioz3190 <ddavid3190@gmail.com>
2017-02-11 15:59:51 +0100

    test menu

--- src/fleurducoeur/FleurDuCoeur.java
+++ src/fleurducoeur/FleurDuCoeur.java
@@ -16,6 +16,7 @@ public class FleurDuCoeur {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        new Accueil().setVisible(true);
     }
     
 }
